7b2d3d54bfcbda50f666b9023d786c5edfc879f2
mmvergnes <mer.vergnes@gmail.com>
2024-03-26 14:44:14 -0300

    creacion de lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Lista-Ma.MercedesVergnes';
+  hasError = true;
+  date = new Date (2024, 2);
+
+  students = [{
+    nombre1: 'Mariano', status1: true,
+    nombre2: 'Cesar', status2: false,
+    nombre3: 'Natalia', status3: true,
+    nombre4: 'Santiago', status4: true,
+    nombre5: 'Josefina', status5: true
+  }];
+
+  viewList = false;
+
+
+  changeError() {
+    this.hasError = !this.hasError;
+  }
+
 }
